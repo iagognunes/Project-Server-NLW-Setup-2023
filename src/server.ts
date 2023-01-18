e1a9975bec3a2d84de61aa8648d03a63292a24af
Iago Guedelha Nunes <iagognunes@outlook.com>
2023-01-18 09:09:30 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+/**
+ * Método HTTP: Get, Post, Put, Delete
+ */
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  });
+  
+  return habits
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("HTTP Server Runing...");
+});
