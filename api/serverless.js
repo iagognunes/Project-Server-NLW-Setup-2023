32d4f395b1d017e5830e9b92d308b92c5e2d0b28
Iago Guedelha Nunes <iagognunes@outlook.com>
2023-01-23 12:06:56 -0300

    Update serverless.js

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/server.ts"), { prefix: '/' });
+app.register(import("../src/server.ts"), { prefix: '/summary' });
 
 export default async (req, res) => {
   await app.ready();
