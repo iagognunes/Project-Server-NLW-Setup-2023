a89e787e17eda3f145b9b5d2a622d6a6336e3d8e
Iago Guedelha Nunes <iagognunes@outlook.com>
2023-01-23 12:18:03 -0300

    funciona pfv

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("src\server.ts"));
+app.register(import("../src/app"));
 
 export default async (req, res) => {
   await app.ready();
